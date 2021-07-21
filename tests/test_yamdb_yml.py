3fba6349106893e561d121b641c3b546ebfd339f
Dvoryanoff <dvoryanoff@mail.ru>
2021-07-22 01:19:09 +0300

    testpassed!

--- tests/test_yamdb_yml.py
+++ tests/test_yamdb_yml.py
@@ -17,7 +17,7 @@ class TestWorkflow:
             re.search(r'on:\s*push:\s*branches:\s*-\smaster', yamdb) or
             'on: [push]' in yamdb or
             'on: push' in yamdb 
-        ), 'Проверьте, что добавили действие при пуше в файл yamdb_workflow.yaml' 
+        ), 'Проверьте, что добавили действие при пуше в файл yamdb_workflow.yaml'
         assert 'pytest' in yamdb, 'Проверьте, что добавили pytest в файл yamdb_workflow.yaml'
         assert 'appleboy/ssh-action' in yamdb, 'Проверьте, что добавили деплой в файл yamdb_workflow.yaml'
         assert 'appleboy/telegram-action' in yamdb, (
